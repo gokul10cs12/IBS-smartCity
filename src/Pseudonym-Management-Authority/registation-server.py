2c64458daa9b247f60e5d5be71cc832f297c8197
gokul.n.b <gokul10cs12@gmail.com>
2020-06-24 02:52:10 +0530

    file arrangement

--- src/Pseudonym-Management-Authority/registation-server.py
+++ src/Pseudonym-Management-Authority/registation-server.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 @app.route('/register')
 def register():
-    return render_template('index.html')
-
+    return render_template('/index.html')
 
 
 @app.route('/success', methods=['GET', 'POST'])
